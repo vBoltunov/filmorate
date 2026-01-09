139c85de50f7d16e0f9b647781d60da9a6c9e7f9
Vitaly Boltunov <vitaliy.boltunov@yandex.ru>
2026-01-09 22:02:39 +0300

    fix: resolve deprecated import warning in FilmControllerTest

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
